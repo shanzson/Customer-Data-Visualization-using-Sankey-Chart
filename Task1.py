c97436f2bedc1eb5e18242ce810ec0b4110ed2e1
Shantanu Sontakke <shantanusontakke015@gmail.com>
2021-07-18 22:07:12 +0530

    Solved lamda error

--- Task1.py
+++ Task1.py
@@ -1,6 +1,7 @@
+
 # coding: utf-8
 
-# In[126]:
+# In[1]:
 
 
 import pandas as pd
@@ -14,12 +15,10 @@ print(data.head(10))
 # Checking if there are NAN values
 print("Presence of NAN values : \n", data.isnull().any())
 
-
 # Function to remove letter from column data
 def remove_letter(val):
-    if (val[0] == 'F' or val[0] == 'T'):
+    if(val[0] == 'F' or val[0] == 'T'):
         return val[1:]
-
 
 data['From'] = data['From'].apply(remove_letter)
 data['To'] = data['To'].apply(remove_letter)
@@ -34,43 +33,47 @@ data['To'] = data['To'].astype(str).astype(int)
 
 print(data.dtypes)
 
-# In[127]:
+
+# In[2]:
 
 
 num_bins = 100
 to_data = data['To']
 legend = ['To']
-plt.hist([to_data], bins=num_bins, color=['Orange'])
+plt.hist([to_data], bins = num_bins, color=['Orange'])
 plt.xlabel("To data")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-# In[128]:
+
+# In[3]:
 
 
 num_bins = 100
 from_data = data['From']
 legend = ['From']
-plt.hist([from_data], bins=num_bins, color=['green'])
+plt.hist([from_data], bins = num_bins, color=['green'])
 plt.xlabel("From data")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-# In[129]:
+
+# In[4]:
 
 
 num_bins = 200
 userid_data = data['userid']
 legend = ['userid']
-plt.hist([userid_data], bins=num_bins, color=['Red'])
+plt.hist([userid_data], bins = num_bins, color=['Red'])
 plt.xlabel("userid")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-# In[130]:
+
+# In[5]:
 
 
 print('Sorting...')
@@ -79,17 +82,19 @@ data.sort_values(['userid', 'From', 'To'], ascending=[True, True, True], inplace
 # grouped.head()
 
 
-# In[131]:
+# In[6]:
 
 
 plt.boxplot(data['To'])
 
-# In[132]:
+
+# In[7]:
 
 
 plt.boxplot(data['From'])
 
-# In[133]:
+
+# In[12]:
 
 
 data = data.sort_values('userid')
@@ -100,17 +105,16 @@ for i in range(99, 210, 5):
 
 print(region_rank)
 
-
 def mark_region(x):
     current_rank = 0
     for i in region_rank:
-        if (x > i):
-            current_rank += 1
-        else:
+        if(x > i):
+            current_rank+=1
+        else: 
             return current_rank
-
-
-# Apply the mark_region function
+    
+    
+# Apply the mark_region function 
 data['source_to'] = data['To'].apply(mark_region)
 data['target_from'] = data['From'].apply(mark_region)
 
@@ -118,13 +122,15 @@ data['target_from'] = data['From'].apply(mark_region)
 print("Presence of NAN values : \n", data.isnull().any())
 data.head(100)
 
-# In[134]:
+
+# In[9]:
 
 
 max_region = data['source_to'].max()
 print(max_region)
 
-# In[135]:
+
+# In[17]:
 
 
 label = []
@@ -155,23 +161,12 @@ for i in range(len(source)):
 
 #################### Sankey Diagram Trial 1##################
 
-# for i in range(0, len(source)):
-#     try:
-#         if source[i] not in output:
-#             output[]
-
-#     except Exception as e:
-#         pass
-
-# for key, value in output['comb_dict'].items():
-#     print('key: ', key, ' value: ', value, '\n')
-
-# def update_source_target(user):
-#     try:
-#         if source in output['links_dict']:
-#             if target_index in output['links_dict'][source_index]:
-
-# grouped.apply(lambda user: update_source_target(user))
+def update_source_target(user):
+    try:
+        print(user)
+    except Exception as e:
+        pass
+data2 = data.apply(lambda x: update_source_target(x['source_to']), axis = 1)
 
 
 # In[137]:
@@ -180,27 +175,51 @@ for i in range(len(source)):
 import plotly.graph_objects as go
 
 node = dict(
-    pad=15,
-    thickness=20,
-    line=dict(color="black", width=0.5),
-    label=label,
-    color="blue"
+    pad = 15,
+    thickness = 20,
+    line = dict(color = "black", width = 0.5),
+    label = label,
+    color = "blue"
 )
 
 fig = go.Figure(data=[go.Sankey(
-    node=dict(
-        pad=15,
-        thickness=20,
-        line=dict(color="black", width=0.5),
-        label=label,
-        color="blue"
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = label,
+      color = "blue"
     ),
-    link=dict(
-        source=source,
-        target=target,
-        value=value
-    ))])
+    link = dict(
+    source = source, 
+    target = target,
+    value = value
+))])
 
 fig.update_layout(autosize=True, title_text="Medium app", font=dict(size=15))
+fig.show()
+    
+
+
+# In[139]:
+
+
+import plotly.graph_objects as go
+
+fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
+      color = "blue"
+    ),
+    link = dict(
+      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = [2, 3, 3, 4, 4, 5],
+      value = [8, 4, 2, 8, 4, 2]
+  ))])
+
+fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
 fig.show()
 
