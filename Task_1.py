37aadace3613ec5ca516247d935cc87ca643f8b9
Shantanu Sontakke <shantanusontakke015@gmail.com>
2021-07-18 12:56:35 +0530

    Added Box plots

--- Task_1.py
+++ Task_1.py
@@ -1,3 +1,9 @@
+
+# coding: utf-8
+
+# In[9]:
+
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -27,15 +33,22 @@ data['To'] = data['To'].astype(str).astype(int)
 
 print(data.dtypes)
 
-# Plotting Histograms
+
+# In[ ]:
+
+
 num_bins = 100
-from_data = data['To']
+to_data = data['To']
 legend = ['To']
 plt.hist([from_data], bins = num_bins, color=['Orange'])
 plt.xlabel("To data")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
+
+
+# In[ ]:
+
 
 num_bins = 100
 from_data = data['From']
@@ -46,11 +59,56 @@ plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-num_bins = 100
-from_data = data['userid']
+
+# In[41]:
+
+
+num_bins = 200
+userid_data = data['userid']
 legend = ['userid']
-plt.hist([from_data], bins = num_bins, color=['Red'])
+plt.hist([userid_data], bins = num_bins, color=['Red'])
 plt.xlabel("userid")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
+
+
+# In[32]:
+
+
+print('Sorting...')
+data.sort_values(['userid', 'From', 'To'], ascending=[True, True, True], inplace=True)
+grouped = data.groupby('userid')
+data.to_csv(r'C:\Users\Shantanu\Desktop\Interview_Bit\Sharechat_code_backup\ShareChat\Intermediate_outputs\grouped_data.csv', index = False)
+grouped.head()
+
+
+# In[4]:
+
+
+plt.boxplot(data['To'])
+
+
+# In[5]:
+
+
+plt.boxplot(data['From'])
+
+
+# In[12]:
+
+
+data = data.sort_values('userid').drop_duplicates('userid')
+data.head()
+
+print('Working...')
+def add_source(x): 
+    # return x['time_event'].rank(method='first').astype(int)
+    print('Calling...')
+    if(151 <= x['To'] <= 200):
+        return 4
+
+# Apply the add_source function 
+# data['source_to'] = data.apply(add_source).reset_index(0, drop=True)
+
+data.head()
