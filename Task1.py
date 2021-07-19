e9391d12915422c5cc511bffc55ed07ff028bb5f
Shantanu Sontakke <shantanusontakke015@gmail.com>
2021-07-19 15:54:11 +0530

    Sankey chart using regions

--- Task1.py
+++ Task1.py
@@ -1,7 +1,6 @@
-
 # coding: utf-8
 
-# In[1]:
+# In[126]:
 
 
 import pandas as pd
@@ -15,10 +14,12 @@ print(data.head(10))
 # Checking if there are NAN values
 print("Presence of NAN values : \n", data.isnull().any())
 
+
 # Function to remove letter from column data
 def remove_letter(val):
-    if(val[0] == 'F' or val[0] == 'T'):
+    if (val[0] == 'F' or val[0] == 'T'):
         return val[1:]
+
 
 data['From'] = data['From'].apply(remove_letter)
 data['To'] = data['To'].apply(remove_letter)
@@ -33,47 +34,43 @@ data['To'] = data['To'].astype(str).astype(int)
 
 print(data.dtypes)
 
-
-# In[2]:
+# In[127]:
 
 
 num_bins = 100
 to_data = data['To']
 legend = ['To']
-plt.hist([to_data], bins = num_bins, color=['Orange'])
+plt.hist([to_data], bins=num_bins, color=['Orange'])
 plt.xlabel("To data")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-
-# In[3]:
+# In[128]:
 
 
 num_bins = 100
 from_data = data['From']
 legend = ['From']
-plt.hist([from_data], bins = num_bins, color=['green'])
+plt.hist([from_data], bins=num_bins, color=['green'])
 plt.xlabel("From data")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-
-# In[4]:
+# In[129]:
 
 
 num_bins = 200
 userid_data = data['userid']
 legend = ['userid']
-plt.hist([userid_data], bins = num_bins, color=['Red'])
+plt.hist([userid_data], bins=num_bins, color=['Red'])
 plt.xlabel("userid")
 plt.ylabel("Frequency")
 plt.legend(legend)
 plt.show()
 
-
-# In[5]:
+# In[130]:
 
 
 print('Sorting...')
@@ -82,39 +79,38 @@ data.sort_values(['userid', 'From', 'To'], ascending=[True, True, True], inplace
 # grouped.head()
 
 
-# In[6]:
+# In[131]:
 
 
 plt.boxplot(data['To'])
 
-
-# In[7]:
+# In[132]:
 
 
 plt.boxplot(data['From'])
 
-
-# In[12]:
+# In[133]:
 
 
 data = data.sort_values('userid')
 
 region_rank = []
-for i in range(99, 210, 5):
+for i in range(99, 210, 15):
     region_rank.append(i)
 
 print(region_rank)
 
+
 def mark_region(x):
-    current_rank = 0
+    current_rank = -1 # As indices start from 0
     for i in region_rank:
-        if(x > i):
-            current_rank+=1
-        else: 
+        if (x > i):
+            current_rank += 1
+        else:
             return current_rank
-    
-    
-# Apply the mark_region function 
+
+
+# Apply the mark_region function
 data['source_to'] = data['To'].apply(mark_region)
 data['target_from'] = data['From'].apply(mark_region)
 
@@ -122,44 +118,13 @@ data['target_from'] = data['From'].apply(mark_region)
 print("Presence of NAN values : \n", data.isnull().any())
 data.head(100)
 
-
-# In[9]:
+# In[134]:
 
 
 max_region = data['source_to'].max()
 print(max_region)
 
-
-# In[27]:
-
-
-label = []
-for i in range(1, max_region + 1):
-    label.append(str(i))
-print(label)
-
-
-
-# Creating source, target and value lists
-source = []
-target = []
-value = []
-
-source = data['source_to'].values.tolist()
-print('Source:\n')
-print(source)
-
-target = data['target_from'].values.tolist()
-print('\n Targets:\n')
-print(target)
-
-print("length of source list is: ", len(source))
-print("length of target list is: ", len(target))
-
-for i in range(len(source)):
-    value.append(1)
-
-#################### Sankey Diagram Trial 1##################
+#################### Computing Source and Target ##################
 
 output = dict()
 
@@ -195,58 +160,68 @@ print('Lambda Function Working... \n\n')
 for key, value in output['links_dict'].items():
     print(key, value)
 
+labels = []
+# for i in range(1, 8):
+#     labels.append(str(i))
+targets = []
+sources = []
+values = []
 
-# In[137]:
+for source_key, source_value in output['links_dict'].items():
+    for target_key, target_value in output['links_dict'][source_key].items():
+        sources.append(source_key)
+        targets.append(target_key)
+        values.append(target_value['unique_users'])
 
+# Check if all users considered
+sum = 0
+for i in values:
+    sum += i
+print(sum)
+
+print('############### Finalizing values #################')
+labels = ['Region One', 'Region Two', 'Region Three',
+           'Region Four', 'Region Five', 'Region Six', 'Region Seven',
+          'Region One', 'Region Two', 'Region Three', 'Region Four',
+         'Region Five', 'Region Six', 'Region Seven']
+# print(labels)
+n = len(sources)
+for i in range(n):
+    if sources[i] > targets[i]:
+        targets[i]+=7
+# print(sources)
+# print(targets)
 
 import plotly.graph_objects as go
+import chart_studio.plotly as py
+import plotly
 
-node = dict(
-    pad = 15,
-    thickness = 20,
-    line = dict(color = "black", width = 0.5),
-    label = label,
-    color = "blue"
-)
+# fig = go.Figure(data=[go.Sankey(
+#     node=dict(
+#         thickness=5,  # default is 20
+#         line=dict(color="black", width=0.05),
+#         label=labels,
+#         color="blue"
+#     ),
+#     link=dict(
+#         source=sources,
+#         target=targets,
+#         value=values
+# ))])
 
 fig = go.Figure(data=[go.Sankey(
     node = dict(
-      pad = 15,
-      thickness = 20,
+      pad = 5,
+      thickness = 10,
       line = dict(color = "black", width = 0.5),
-      label = label,
+      label = labels,
       color = "blue"
     ),
     link = dict(
-    source = source, 
-    target = target,
-    value = value
-))])
-
-fig.update_layout(autosize=True, title_text="Medium app", font=dict(size=15))
-fig.show()
-    
-
-
-# In[139]:
-
-
-import plotly.graph_objects as go
-
-fig = go.Figure(data=[go.Sankey(
-    node = dict(
-      pad = 15,
-      thickness = 20,
-      line = dict(color = "black", width = 0.5),
-      label = ["A1", "A2", "B1", "B2", "C1", "C2"],
-      color = "blue"
-    ),
-    link = dict(
-      source = [0, 1, 0, 2, 3, 3], # indices correspond to labels, eg A1, A2, A1, B1, ...
-      target = [2, 3, 3, 4, 4, 5],
-      value = [8, 4, 2, 8, 4, 2]
+      source = sources, # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = targets,
+      value = values
   ))])
 
-fig.update_layout(title_text="Basic Sankey Diagram", font_size=10)
+fig.update_layout(autosize=True, title_text="Medium app", font=dict(size=15), plot_bgcolor='white')
 fig.show()
-
